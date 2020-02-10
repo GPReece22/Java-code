31d649806f2a26b351454e2949a7246583839d7f
GPReece22 <56441624+GPReece22@users.noreply.github.com>
2020-02-10 12:43:43 +0000

    Add files via upload

---
+++ Term2/Modelling Practice/Week16/PhoneBookController.java
@@ -0,0 +1,21 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+
+public class PhoneBookController {
+    public ChoiceBox addNameChoiceBox;
+    public ChoiceBox addNumberChoiceBox;
+    public Button addNewEntryButton;
+    public Button getNumberButton;
+    public TextField inputNameTextField;
+    public TextField getNumberTextfield;
+
+    public void addNewEntry(ActionEvent actionEvent) {
+    }
+
+    public void getNumber(ActionEvent actionEvent) {
+    }
+}
